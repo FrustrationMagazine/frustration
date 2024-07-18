42270f989fe68034a38323167d2e8215c387d9b2
Thibaut Izard <thibaut.izard@gmail.com>
2024-07-18 11:18:39 +0200

    🎨 style(dashboard): Improve sidenav appearance

--- apps/dashboard/app/dashboard/_components/Sidenav.tsx
+++ apps/dashboard/app/dashboard/_components/Sidenav.tsx
@@ -2,39 +2,51 @@ import React, { ReactNode } from "react";
 import Link from "next/link";
 import { FaCreditCard } from "react-icons/fa";
 import { MdUpdate } from "react-icons/md";
-import { poppins } from "@/utils/fonts";
+import { poppins } from "@dashboard/fonts";
 import SignOut from "./SignOut";
+import { Separator } from "@/ui/components/separator";
 
-const SIDENAV_ELEMENTS = [
+type SidenavElement = {
+  label: string;
+  icon: ReactNode;
+  href: string;
+  key: string;
+};
+
+const SIDENAV_ELEMENTS: SidenavElement[] = [
   {
     label: "Revenus",
     icon: <FaCreditCard />,
     href: "/dashboard/income",
+    key: "income",
   },
   {
     label: "Mises à jour",
     icon: <MdUpdate />,
     href: "/dashboard/update",
+    key: "update",
   },
 ];
 
-const MenuDivider = <hr className='border-3 border-frustration-yellow-hover' />;
-
-const Menu = ({ label, icon, href }: { label: string; icon: ReactNode; href: string }) => {
-  return (
-    <li key={label.toLowerCase()}>
-      <Link
-        href={href}
-        className={`mb-2 flex items-center gap-4 px-5 py-2 text-lg ${poppins.className} rounded-md transition duration-500 hover:bg-frustration-yellow-hover`}
-      >
-        {icon} {label}
-      </Link>
-      {MenuDivider}
-    </li>
-  );
-};
-
-const Menus = <ul className='w-full px-3 text-frustration-yellow'>{SIDENAV_ELEMENTS.map(Menu)}</ul>;
+const Menus = (
+  <ul className='w-full space-y-2 px-3 text-frustration-yellow'>
+    {SIDENAV_ELEMENTS.map(({ label, icon, href, key }) => {
+      return (
+        <React.Fragment key={key}>
+          <li>
+            <Link
+              href={href}
+              className={`flex items-center gap-4 px-5 py-2 text-lg ${poppins.className} rounded-md transition duration-500 hover:bg-frustration-yellow-hover`}
+            >
+              {icon} {label}
+            </Link>
+          </li>
+          <Separator className='bg-frustration-yellow-hover' />
+        </React.Fragment>
+      );
+    })}
+  </ul>
+);
 
 /*********************************/
 /*           EXPORT              */
