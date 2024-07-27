c5fb061708d39afe294520aa30720282f0ad986b
Thibaut Izard <thibaut.izard@gmail.com>
2024-07-27 13:13:39 +0200

    ✨ feat(dashboard): Add dialog alert for sign out button

--- apps/dashboard/src/app/dashboard/_components/SignOut.tsx
+++ apps/dashboard/src/app/dashboard/_components/SignOut.tsx
@@ -1,16 +1,43 @@
+"use client";
+
 import React from "react";
 import { Button } from "@/ui/components/button";
 import { signOutDashboard } from "../_actions/signOut";
 import { RiLogoutBoxLine } from "react-icons/ri";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/ui/components/alert-dialog";
 
 const SignOut = () => {
   return (
-    <form action={signOutDashboard}>
-      <Button variant='secondary' type='submit' className='flex items-center gap-2'>
-        <RiLogoutBoxLine />
-        <span>Se déconnecter</span>
-      </Button>
-    </form>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant='secondary' className='flex items-center gap-2'>
+          <RiLogoutBoxLine />
+          <span>Se déconnecter</span>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Voulez-vous vraiment vous déconnecter ?</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Annuler</AlertDialogCancel>
+          <form action={signOutDashboard}>
+            <AlertDialogAction asChild>
+              <Button type='submit'>Se déconnecter</Button>
+            </AlertDialogAction>
+          </form>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
