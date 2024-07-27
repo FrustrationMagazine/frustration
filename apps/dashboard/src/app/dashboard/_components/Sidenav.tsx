250c96e46dac7b72f0d94952367abc4820c8f304
Thibaut Izard <thibaut.izard@gmail.com>
2024-07-27 12:54:46 +0200

    🎨 style(dashboard): Improve sidenav with current path highlight

--- apps/dashboard/src/app/dashboard/_components/Sidenav.tsx
+++ apps/dashboard/src/app/dashboard/_components/Sidenav.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import Link from "next/link";
 import { FaCreditCard } from "react-icons/fa";
@@ -5,6 +7,8 @@ import { MdUpdate } from "react-icons/md";
 import { poppins } from "../../../utils/fonts";
 import SignOut from "./SignOut";
 import { Separator } from "@/ui/components/separator";
+import { usePathname } from "next/navigation";
+import { cn } from "@/ui/lib/utils";
 
 type SidenavElement = {
   label: string;
@@ -28,30 +32,35 @@ const SIDENAV_ELEMENTS: SidenavElement[] = [
   },
 ];
 
-const Menus = (
-  <ul className='w-full space-y-2 px-3 text-frustration-yellow'>
-    {SIDENAV_ELEMENTS.map(({ label, icon, href, key }) => {
-      return (
-        <React.Fragment key={key}>
-          <li>
-            <Link
-              href={href}
-              className={`flex items-center gap-4 px-5 py-2 text-lg ${poppins.className} rounded-md transition duration-500 hover:bg-frustration-yellow-hover`}
-            >
-              {icon} {label}
-            </Link>
-          </li>
-          <Separator className='bg-frustration-yellow-hover' />
-        </React.Fragment>
-      );
-    })}
-  </ul>
-);
-
 /*********************************/
 /*           EXPORT              */
 /*********************************/
 const Sidenav = () => {
+  const currentPath = usePathname();
+
+  const Menus = (
+    <ul className='w-full space-y-2 px-3 text-frustration-yellow'>
+      {SIDENAV_ELEMENTS.map(({ label, icon, href, key }) => {
+        return (
+          <React.Fragment key={key}>
+            <li>
+              <Link
+                href={href}
+                className={cn(
+                  `flex items-center gap-4 px-5 py-2 text-lg ${poppins.className} rounded-md transition duration-500`,
+                  href === currentPath && "bg-frustration-yellow-hover",
+                )}
+              >
+                {icon} {label}
+              </Link>
+            </li>
+            <Separator className='bg-frustration-yellow-hover' />
+          </React.Fragment>
+        );
+      })}
+    </ul>
+  );
+
   return (
     <aside className='flex w-60 flex-col items-center justify-between bg-black pb-4'>
       {Menus}
