8e294c5af7e0610e4ffb5bd0457336aa1a7a1ecb
Thibaut Izard <thibaut.izard@gmail.com>
2024-08-14 22:51:03 +0200

    🎨 style(dashboard): Add loader for income page

---
+++ apps/dashboard/src/app/dashboard/income/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SuperBallsLoader from "@dashboard/components/SuperBallsLoader/SuperBallsLoader";
+
+export default () => {
+  return (
+    <div className='m-auto'>
+      <SuperBallsLoader />
+    </div>
+  );
+};
