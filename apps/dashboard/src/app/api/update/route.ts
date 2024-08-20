b2d85d1ee481598c123f72dff6adf5cf4e63e2c2
Thibaut Izard <thibaut.izard@gmail.com>
2024-08-20 10:04:11 +0200

    🐛 fix(dashboard): Fix cron job for updating dashboard

--- apps/dashboard/src/app/api/update/route.ts
+++ apps/dashboard/src/app/api/update/route.ts
@@ -1,10 +1,7 @@
-import { waitUntil } from "@vercel/functions";
 import { updateTransactions, updateStripeBalance } from "../../dashboard/update/_actions";
 
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
-
-export function GET(request: Request) {
-  waitUntil(updateTransactions({ updateMethod: "smart" }));
-  waitUntil(updateStripeBalance());
+export async function GET() {
+  await updateTransactions({ updateMethod: "smart" });
+  await updateStripeBalance();
   return new Response(`Updated dashboard from region : ${process.env.VERCEL_REGION}`);
 }
