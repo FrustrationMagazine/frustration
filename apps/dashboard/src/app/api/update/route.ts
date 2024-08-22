08521bdc0c999f0ab3a05bd6f032256e27e6cc8b
Thibaut Izard <thibaut.izard@gmail.com>
2024-08-22 08:18:06 +0200

    🧰 config(dashboard): Update cron job

--- apps/dashboard/src/app/api/update/route.ts
+++ apps/dashboard/src/app/api/update/route.ts
@@ -1,5 +1,8 @@
 import { updateTransactions, updateStripeBalance } from "../../dashboard/update/_actions";
 
+export const dynamic = "force-dynamic";
+export const maxDuration = 60;
+
 export async function GET() {
   await updateTransactions({ updateMethod: "smart" });
   await updateStripeBalance();
