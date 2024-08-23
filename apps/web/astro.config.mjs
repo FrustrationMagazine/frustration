dd4e96fef07c07798097462808c1b87b23d660d3
Thibaut Izard <thibaut.izard@gmail.com>
2024-08-23 08:11:38 +0200

    🧰 config(web): Add vercel web analytics

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [react(), tailwind()],
   image: {
     domains: ["i0.wp.com"],
