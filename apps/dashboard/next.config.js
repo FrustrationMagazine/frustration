d7000a15499a45af3f16699fb9ba98a5ab6b38b8
Thibaut Izard <thibaut.izard@gmail.com>
2024-08-31 21:59:02 +0200

    🐛 fix(dashboard): next prisma bug fixed

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
+// Check https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-monorepo if you want more information about why this is needed
+
 const nextConfig = {
+   webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
