a816856cb737c0461023455fded3eddbc3845c75
Thibaut Izard <thibaut.izard@gmail.com>
2024-09-01 10:39:56 +0200

    🔨 refactor(web): Large refactorings

--- packages/utils/files.ts
+++ packages/utils/files.ts
@@ -1,9 +1,6 @@
 export function createCSVinURL<T>(headers: string[], rows: T[]): string {
   const CSV_SEPARATOR = ";";
-  const csvContent = [
-    headers.join(CSV_SEPARATOR),
-    ...rows.map((row: any) => Object.values(row).join(CSV_SEPARATOR)),
-  ].join("\n");
+  const csvContent = [headers.join(CSV_SEPARATOR), ...rows.map((row: any) => Object.values(row).join(CSV_SEPARATOR))].join("\n");
   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
   const CSVinURL = URL.createObjectURL(blob);
   return CSVinURL;
