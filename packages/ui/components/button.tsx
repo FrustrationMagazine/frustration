a3f7dd69ed021f279d33e2c4597fed2e837b4b55
Thibaut Izard <thibaut.izard@gmail.com>
2024-09-06 11:40:55 +0200

    🎨 style(dashboard): Resize button

--- packages/ui/components/button.tsx
+++ packages/ui/components/button.tsx
@@ -21,6 +21,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-12 rounded-md px-8 text-base",
         icon: "h-9 w-9"
       }
     },
