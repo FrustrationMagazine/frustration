68be802b67db8fc83efa35fa583f6f20bfa63d6e
Thibaut Izard <thibaut.izard@gmail.com>
2025-01-27 22:30:03 +0100

    ✨ feat(web): Search from on homepage

---
+++ apps/web/src/layout/BaseLayout/Header/_components/SearchOverlay.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
+
+import { cn } from "@/utils/tailwind";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function SearchOverlay({ children }: Props) {
+  const [search, setSearch] = React.useState("");
+  const [opened, setOpened] = React.useState(false);
+
+  const redirectPageResults = () => {
+    console.log("redirectPageResults", search);
+    setSearch("");
+  };
+  return (
+    <>
+      <button
+        type="button"
+        title="Recherche"
+        className="w-4 md:w-5 xl:w-6"
+        onClick={() => setOpened(true)}
+        aria-label="Recherche">
+        <FaMagnifyingGlass size="100%" />
+      </button>
+      <div
+        className={cn(
+          "absolute left-0 top-0 h-0 w-screen overflow-hidden bg-black transition-all duration-1000",
+          opened && "h-screen",
+        )}>
+        <button
+          type="button"
+          title="Fermer"
+          className={cn(
+            "absolute right-5 top-5 opacity-0 transition-opacity duration-1000",
+            opened && "opacity-100",
+          )}
+          onClick={() => setOpened(false)}
+          aria-label="Fermer">
+          <IoCloseSharp size={72} />
+        </button>
+        <form
+          action={redirectPageResults}
+          className={cn(
+            "absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-8 opacity-0 transition-opacity duration-1000",
+            opened && "opacity-100",
+          )}>
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            className="color-yellow w-[600px] max-w-[90vw] border-4 border-dashed border-yellow bg-black px-4 py-2 text-3xl font-bold"
+          />
+          <button
+            className="rounded-full bg-yellow px-6 py-2 font-bakbak text-3xl text-black transition-opacity duration-300 disabled:opacity-20"
+            type="submit"
+            disabled={!search}>
+            Rechercher
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default SearchOverlay;
