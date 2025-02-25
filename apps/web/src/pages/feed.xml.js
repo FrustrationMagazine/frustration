7212ce34800d1e4daefdff5993e6cdf75e6bf8db
Thibaut Izard <thibaut.izard@gmail.com>
2025-02-25 10:03:56 +0100

    ✨ feat(web): Custom image for RSS feed

--- apps/web/src/pages/feed.xml.js
+++ apps/web/src/pages/feed.xml.js
@@ -7,7 +7,6 @@ export async function GET(context) {
     title: post.title,
     author: post.author.node.name,
     description: post.excerpt,
-    content: post.content,
     categories: post.categories.nodes.map(({ name }) => name),
     pubDate: new Date(post.date),
     link: `/${post.slug}`,
@@ -19,6 +18,13 @@ export async function GET(context) {
       "Frustration Magazine, média indépendant et offensif qui relate la guerre des classes.",
     site: context.site,
     items,
-    customData: `<language>fr-fr</language>`,
+    customData: `
+     <language>fr-fr</language>
+     <image>
+        <url>${context.site.origin}/img/favicon_light.png</url>
+        <title>Frustration Magazine</title>
+        <link>${context.site.origin}</link>
+      </image>
+    `,
   });
 }
